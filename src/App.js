c8d8ded7dd4d64dbd08754591d7d4a14286d790e
Kris Thornton <Kristhornton@ymail.com>
2023-05-08 20:44:51 -0700

    Finished project with interal component in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import axios from "axios"
+import axios from "axios";
 
 
 function App() {
   const [data, setData] = useState({});
   const [query, setQuery] = useState("");
-
+  const [image, setImage] = useState({})
 
   useEffect(() => {
   const fetchData = () => {
     axios
-      .get("https://api.nasa.gov/#apod")
+      .get("https://api.nasa.gov/planetary/apod?api_key=A2OpCJGYHwZcJpHYKAdOxHKQAHNaSjfblhGWSA97")
       .then ((res) => {
-        console.log(res)
+        setData(res.data) 
+        console.log(res.data)
       })
+      .catch(err => console.error(err))
   }
-      
+      fetchData()
 }, [])
+
+
+
+
+const ChangePhoto = (props) => {
+  return (
+    
+    <div className = "PhotoStyle">
+     <button>Date Back</button>
+    <button>Date Forward</button>
+    <h2>{props.photo.title}</h2>
+    <p>{props.photo.date}</p>
+    <img src={props.photo.hdurl} className="nasa-photo"/>
+    </div>
+  )
+}
+
+
+
 
 
   return (
@@ -29,8 +50,11 @@ function App() {
       </header>
       </div>
 
-    <div>
-      <img src="https://api.nasa.gov/#apod" alt="Nasa photo" />
+    <div >
+      {/* <img src = {data.url} className = 'PhotoStyle' alt={data.title}/>
+      <p className='DataInfo'> {data.date} - {data.explanation}</p> */}
+      {<ChangePhoto photo={data} />}
+
     </div>
 
     <footer>Thank you for taking a look.</footer>
